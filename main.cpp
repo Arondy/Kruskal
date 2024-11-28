90b65f75085e0a8a2669af000f8e30c28cbc9263
arondy <antongatcenko@gmail.com>
2024-11-28 17:55:50 +0300

    Оптимизирована генерация случайного связного графа. На наиболее затратный цикл добавлен omp parallel for. Сложность генерации - n^3. Полный граф с 1000 вершин строится за 0.4 секунды на 6c/12t.

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 #include <omp.h>
 
 #define MIN_WEIGHT 1
-#define MAX_WEIGHT 10
-#define VERTICES_NUM 1e2
+#define MAX_WEIGHT 1e3
+#define VERTICES_NUM 1e3
 #define EDGES_NUM VERTICES_NUM * (VERTICES_NUM - 1) / 2
 
 #define SEED 920215
@@ -23,6 +23,10 @@ public:
     int v;
     int weight;
 
+    Edge() = default;
+
+    Edge(const int u, const int v, const int weight): u{u}, v{v}, weight{weight}{}
+
     bool operator<(Edge const &other) const{
         return weight < other.weight;
     }
@@ -32,8 +36,8 @@ public:
         return os;
     }
 
-    [[nodiscard]] bool matches(const int &U, const int &V) const{
-        return u == U && v == V;
+    bool operator==(Edge const &other) const{
+        return u == other.u && v == other.v;
     }
 };
 
@@ -75,9 +79,8 @@ vector<Edge> generate_random_connected_graph(const int &V, const int &E, const i
     if (E > V * (V - 1) / 2){
         throw std::invalid_argument("It is impossible to create a graph: there are too many edges!");
     }
-
+    const double start = omp_get_wtime();
     std::mt19937 gen(seed);
-    std::uniform_int_distribution<> vertex_dist(0, V - 1);
     std::uniform_int_distribution<> weight_dist(MIN_WEIGHT, MAX_WEIGHT);
     vector<Edge> edges;
     vector<int> vertices(V);
@@ -94,25 +97,43 @@ vector<Edge> generate_random_connected_graph(const int &V, const int &E, const i
         int u = vertices[i - 1];
         int v = vertices[i];
         int weight = weight_dist(gen);
-        edges.push_back({u, v, weight});
-    }
 
-    // Добавление дополнительных рёбер
-    while (edges.size() < E){
-        int u = vertex_dist(gen);
-        int v = vertex_dist(gen);
-
-        if (u == v){
-            continue;
-        }
-
-        for (const Edge &e: edges){
-            if (e.matches(min(u, v), max(u, v))){
-                int weight = weight_dist(gen);
-                edges.push_back({u, v, weight});
-            }
+        if (u < v){
+            edges.emplace_back(u, v, weight);
+        } else{
+            edges.emplace_back(v, u, weight);
         }
     }
+
+    if (edges.size() == E){
+        return edges;
+    }
+
+    // Генерация всех возможных рёбер
+    vector<Edge> all_edges;
+    for (int u = 0; u < V; ++u){
+        for (int v = u + 1; v < V; ++v){
+            int weight = weight_dist(gen);
+            all_edges.emplace_back(u, v, weight);
+        }
+    }
+
+    // Удаление остовных ребер
+#pragma omp parallel for num_threads(omp_get_num_procs())
+    for (const Edge &edge: edges){
+        if (auto it = std::find(all_edges.begin(), all_edges.end(), edge); it != all_edges.end()){
+            all_edges.erase(it);
+        }
+    }
+
+    // Перемешиваем все рёбра
+    std::shuffle(all_edges.begin(), all_edges.end(), gen);
+
+    // Копируем нужное количество рёбер
+    int needed_edges = E - edges.size();
+    edges.insert(edges.end(), all_edges.begin(), all_edges.begin() + needed_edges);
+    const double end = omp_get_wtime();
+    cout << "Generation took " << end - start << " seconds for " << edges.size() << " edges" << endl;
 
     return edges;
 }
@@ -150,7 +171,11 @@ vector<Edge> findMST_Kruskal_t(vector<Edge> &edges, const int &_){
     return findMST_Kruskal(edges);
 }
 
-vector<Edge> findMST_ParallelKruskal(vector<Edge> &edges, const int &threads_num){}
+vector<Edge> findMST_ParallelKruskal(vector<Edge> &edges, const int &threads_num){
+    vector<Edge> result;
+
+    return result;
+}
 
 void time_algorithm(const int &min_threads_num, const int &max_threads_num,
                     std::function<vector<Edge>(vector<Edge> &, const int &)> func){
