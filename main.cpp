ee3ec5eb5cd863bcca8d146d7af7cd28e309817f
arondy <antongatcenko@gmail.com>
2024-11-28 19:14:20 +0300

    Оптимизировал генерацию графа для точно связного по количеству ребер. Добавил ТЗ.

--- main.cpp
+++ main.cpp
@@ -85,44 +85,51 @@ vector<Edge> generate_random_connected_graph(const int &V, const int &E, const i
     vector<Edge> edges;
     vector<int> vertices(V);
 
-    for (int i = 0; i < V; ++i){
-        vertices[i] = i;
-    }
+    if constexpr (EDGES_NUM < VERTICES_NUM * (VERTICES_NUM - 1) / 2 - (VERTICES_NUM - 1)){
+        for (int i = 0; i < V; ++i){
+            vertices[i] = i;
+        }
 
-    // Перемешиваем вершины для случайного соединения
-    std::shuffle(vertices.begin(), vertices.end(), gen);
+        // Перемешиваем вершины для случайного соединения
+        std::shuffle(vertices.begin(), vertices.end(), gen);
 
-    // Создаём V-1 рёбер для остовного дерева
-    for (int i = 1; i < V; ++i){
-        int u = vertices[i - 1];
-        int v = vertices[i];
-        int weight = weight_dist(gen);
+        // Создаём V-1 рёбер для остовного дерева
+        for (int i = 1; i < V; ++i){
+            int u = vertices[i - 1];
+            int v = vertices[i];
+            int weight = weight_dist(gen);
 
-        if (u < v){
-            edges.emplace_back(u, v, weight);
-        } else{
-            edges.emplace_back(v, u, weight);
+            if (u < v){
+                edges.emplace_back(u, v, weight);
+            } else{
+                edges.emplace_back(v, u, weight);
+            }
+        }
+
+        if (edges.size() == E){
+            return edges;
         }
     }
 
-    if (edges.size() == E){
-        return edges;
-    }
-
     // Генерация всех возможных рёбер
-    vector<Edge> all_edges;
+    vector<Edge> all_edges(VERTICES_NUM * (VERTICES_NUM - 1) / 2);
     for (int u = 0; u < V; ++u){
         for (int v = u + 1; v < V; ++v){
             int weight = weight_dist(gen);
-            all_edges.emplace_back(u, v, weight);
+            int index = u * (V - 1) - u * (u + 1) / 2 + v - u - 1;
+            all_edges[index] = Edge{u, v, weight};
         }
     }
 
     // Удаление остовных ребер
+    omp_lock_t lock;
+    omp_init_lock(&lock);
 #pragma omp parallel for num_threads(omp_get_num_procs())
     for (const Edge &edge: edges){
         if (auto it = std::find(all_edges.begin(), all_edges.end(), edge); it != all_edges.end()){
+            omp_set_lock(&lock);
             all_edges.erase(it);
+            omp_unset_lock(&lock);
         }
     }
 
@@ -142,6 +149,8 @@ vector<Edge> findMST_Kruskal(vector<Edge> &edges){
     vector<int> parent(EDGES_NUM);
     vector<int> rank(EDGES_NUM);
     vector<Edge> result;
+
+    cout << "Consecutive Kruskal:" << endl;
 
     // Создаем подмножество из каждой вершины
     for (int i = 0; i < EDGES_NUM; i++){
@@ -174,6 +183,8 @@ vector<Edge> findMST_Kruskal_t(vector<Edge> &edges, const int &_){
 vector<Edge> findMST_ParallelKruskal(vector<Edge> &edges, const int &threads_num){
     vector<Edge> result;
 
+    cout << "Parallel Kruskal:" << endl;
+
     return result;
 }
 
@@ -198,18 +209,5 @@ void time_algorithm(const int &min_threads_num, const int &max_threads_num,
 }
 
 int main(){
-    const int threads_num = omp_get_num_procs(); // for future parallel algo
-    // vector<Edge> edges = generate_random_connected_graph(VERTICES_NUM, EDGES_NUM, SEED);
-    // vector<Edge> result = findMST_Kruskal(edges);
-    //
-    // int resulting_weight = 0;
-    // cout << "MST:" << endl;
-    //
-    // for (const Edge &e: result){
-    //     cout << e << endl;
-    //     resulting_weight += e.weight;
-    // }
-    //
-    // cout << "Resulting weight: " << resulting_weight << endl;
     time_algorithm(1, 1, findMST_Kruskal_t);
 }
